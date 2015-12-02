54d85461d9c841633c9b6bed26d412f1892cf49f
Sai Krishna <saikrishna321@yahoo.com>
2015-12-02 18:59:25 +0530

    fixed style

--- src/main/java/com/report/factory/ExtentManager.java
+++ src/main/java/com/report/factory/ExtentManager.java
@@ -4,13 +4,12 @@ import com.relevantcodes.extentreports.ExtentReports;
 
 public class ExtentManager {
 	private static ExtentReports instance;
-	
+
 	public static synchronized ExtentReports getInstance() {
 		if (instance == null) {
 			System.out.println(System.getProperty("user.dir"));
 			instance = new ExtentReports(System.getProperty("user.dir") + "/ExtentReport.html");
 		}
-		
 		return instance;
 	}
 }
