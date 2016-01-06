c28480247ffc3b35716f1c6388e9e0e991809a6d
Sai Krishna <saikrishna321@yahoo.com>
2016-01-06 19:21:10 +0530

    fixed thread pool for parallel run

--- src/main/java/com/appium/manager/AppiumParallelTest.java
+++ src/main/java/com/appium/manager/AppiumParallelTest.java
@@ -68,7 +68,7 @@ public class AppiumParallelTest extends TestListenerAdapter {
 			thread_device_count = Integer.valueOf(Thread.currentThread().getName().split("-")[3]) - 1;
 		} else if (prop.getProperty("RUNNER").equalsIgnoreCase("parallel")) {
 			System.out.println("******Into Parallel BaseTest*******" + Thread.currentThread().getName().split("-")[1]);
-			thread_device_count = Integer.valueOf(Thread.currentThread().getName().split("-")[1]) - 1;
+			thread_device_count = Integer.valueOf(Thread.currentThread().getName().split("-")[1]);
 			System.out.println("Device received from array" + devices.get(thread_device_count));
 		}
 
