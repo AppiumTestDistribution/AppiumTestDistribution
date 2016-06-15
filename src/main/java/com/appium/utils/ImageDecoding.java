185a886baddd888f23db1b6ad820de43fae50fe5
Sai Krishna <saikrishna321@yahoo.com>
2016-06-15 11:08:07 +0530

    support to add image as base64

---
+++ src/main/java/com/appium/utils/ImageDecoding.java
@@ -0,0 +1,31 @@
+package com.appium.utils;
+
+import sun.misc.BASE64Encoder;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
+/**
+ * Created by saikrisv on 10/06/16.
+ */
+public class ImageDecoding {
+    public static String encodeToString(BufferedImage image, String type) {
+        String imageString = null;
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+
+        try {
+            ImageIO.write(image, type, bos);
+            byte[] imageBytes = bos.toByteArray();
+
+            BASE64Encoder encoder = new BASE64Encoder();
+            imageString = encoder.encode(imageBytes);
+
+            bos.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return imageString;
+    }
+}
