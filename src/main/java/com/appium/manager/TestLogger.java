98349a8f7e7cadf8c65ce6d27cc64898b43bf473
dkjha52 <dkjha52@hotmail.com>
2017-02-03 20:12:33 +0530

    Fixed Travis CI build

--- src/main/java/com/appium/manager/TestLogger.java
+++ src/main/java/com/appium/manager/TestLogger.java
@@ -208,16 +208,16 @@ class TestLogger {
                                 + "_failed_" + result.getMethod().getMethodName() + "_framed.png");
                 if (framedImageAndroid.exists()) {
                     log.addScreenCaptureFromPath(
-                            System.getProperty("user.dir") 
-                            + "/target/screenshot/android/" + device_udid.replaceAll("\\W", "_") + "/"
+                            System.getProperty("user.dir") + "/target/screenshot/android/" 
+                            + device_udid.replaceAll("\\W", "_") + "/"
                                     + className + "/" + result.getMethod().getMethodName()
                                     + "/" + screenShotNameWithTimeStamp
                                     + deviceModel + "_failed_" + result
                                     .getMethod().getMethodName() + "_framed.png");
                 } else {
                     log.addScreenCaptureFromPath(
-                            System.getProperty("user.dir") 
-                            + "/target/screenshot/android/" + device_udid.replaceAll("\\W", "_") + "/"
+                            System.getProperty("user.dir") + "/target/screenshot/android/" 
+                            + device_udid.replaceAll("\\W", "_") + "/"
                                     + className + "/" + result.getMethod().getMethodName() + "/"
                                     + screenShotNameWithTimeStamp + deviceModel + "_" + result
                                     .getMethod().getMethodName() + "_failed.png");
