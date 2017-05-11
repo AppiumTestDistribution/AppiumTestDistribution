7529eab6ff9cd3c3facf94d1464f976461ca2161
Kiran <kiransinbox1@gmail.com>
2017-05-11 09:54:21 +0530

    Small text change

--- webContent/app/src/App.js
+++ webContent/app/src/App.js
@@ -8,11 +8,11 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Appium Test Report</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1 className="App-intro">
+          Comming Soon
+        </h1>
       </div>
     );
   }
