f36a28cdfca99abe7865c89a4427f8eda0336e3b
bsneha <bsneha@thoughtworks.com>
2017-05-17 13:22:45 +0530

    Kill the website if running already

--- generate_report.sh
+++ generate_report.sh
@@ -1,5 +1,8 @@
 ./screenshot_copy.sh
+echo "Kill all processes running on PORT: 3000"
+sudo lsof -t -i tcp:3000 -s tcp:listen | sudo xargs kill
 echo "Installing node packages."
 $(cd webContent/app && npm install)
 echo "Start Website"
 $(cd webContent/app && npm start)
+
