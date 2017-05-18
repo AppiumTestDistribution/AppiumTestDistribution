38767b48309c53664770f6595743b7e6b2c7748f
Kiran <kiransinbox1@gmail.com>
2017-05-18 10:14:32 +0530

    Ui fixes and content ingeration

--- webContent/app/src/components/reportCard/ReportCard.jsx
+++ webContent/app/src/components/reportCard/ReportCard.jsx
@@ -1,35 +1,36 @@
 import React from 'react';
-import { Card, CardHeader, CardTitle, CardText } from 'material-ui/Card';
+import { Card, CardHeader, CardTitle } from 'material-ui/Card';
 import { Col } from 'react-flexbox-grid';
 
 const styles = {
   card: {
     margin: 20
   },
+  imageWrapper: {
+    textAlign: 'center',
+  },
   image: {
-    textAlign: 'center'
+    width: '80%',
   }
 }
 
 const ReportCard = ({ content }) => {
   let screenShotPath = content.screenShot.split('/target')[1]
+  console.log(content)
   return (
     <Col sm={12} md={6} lg={4}>
-      <Card style={styles.card}>
+      <Card style={styles.card} >
+        <CardTitle title={content.methodName} subtitle={`Device UUId: ${content.info.deviceUDID}`} 
+           subtitle={`Model: ${content.info.deviceModel}`}/>
         <CardHeader
-          title={content.info.deviceName}
-          subtitle={content.info.deviceModel}
+          titleColor={'#444444'}
+          title={`Device OS:  ${content.info.deviceName}, Verion: ${content.info.deviceOS}`}
+          subtitle={`Device UUId: ${content.info.deviceUDID}`}
+         
         />
-        <div style={styles.image}>
-          <img src={screenShotPath} alt="screenshot" />
+        <div style={styles.imageWrapper}>
+          <img style={styles.image}src={screenShotPath} alt="screenshot" />
         </div>
-        <CardTitle title="Card title" subtitle="Card subtitle" />
-        <CardText>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-        Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi.
-        Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque.
-        Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.
-      </CardText>
       </Card>
     </Col>
   )
