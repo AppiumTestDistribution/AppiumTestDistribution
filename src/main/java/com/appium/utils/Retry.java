5790451281205e39fb8b6cc634268a59afb6c0dc
sai krishna <saikrishna321@yahoo.com>
2017-06-22 22:13:28 +0530

    fixed retry on failure

---
+++ src/main/java/com/appium/utils/Retry.java
@@ -0,0 +1,59 @@
+package com.appium.utils;
+
+import com.annotation.values.RetryCount;
+import com.appium.manager.ConfigFileManager;
+import com.appium.manager.ReportManager;
+import org.testng.IRetryAnalyzer;
+import org.testng.ITestResult;
+
+import java.io.IOException;
+import java.lang.reflect.Method;
+
+public class Retry implements IRetryAnalyzer {
+    private int retryCount = 0;
+    private int maxRetryCount;
+    private ConfigFileManager prop;
+    private ReportManager reportManager;
+
+    public Retry() {
+        try {
+            prop = ConfigFileManager.getInstance();
+            reportManager = new ReportManager();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override public boolean retry(ITestResult iTestResult) {
+        Method[] methods = iTestResult.getInstance().getClass().getMethods();
+        for (Method m : methods) {
+            if (m.getName().equals(iTestResult.getMethod().getMethodName())) {
+                if (m.isAnnotationPresent(RetryCount.class)) {
+                    RetryCount ta = m.getAnnotation(RetryCount.class);
+                    maxRetryCount = ta.maxRetryCount();
+                } else {
+                    try {
+                        maxRetryCount = Integer.parseInt(prop.getProperty("MAX_RETRY_COUNT"));
+                    } catch (Exception e) {
+                        maxRetryCount = 0;
+                    }
+                }
+            }
+        }
+        if (iTestResult.getStatus() == ITestResult.FAILURE) {
+            System.out.println("Test Failed");
+            if (retryCount == maxRetryCount) {
+                System.out.println("Log report");
+                //reportManager.setAuthorName(iTestResult.getMethod().getMethodName());
+            }
+            if (retryCount < maxRetryCount) {
+                retryCount++;
+                System.out.println(
+                        "Retrying Failed Test Cases " + retryCount + "out of " + maxRetryCount);
+                return true;
+
+            }
+        }
+        return false;
+    }
+}
---
+++ src/main/java/com/appium/utils/RetryListener.java
@@ -0,0 +1,25 @@
+package com.appium.utils;
+
+import org.testng.IAnnotationTransformer;
+import org.testng.IRetryAnalyzer;
+import org.testng.annotations.ITestAnnotation;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
+
+/**
+ * Created by saikrisv on 22/06/17.
+ */
+public class RetryListener implements IAnnotationTransformer {
+
+
+    @Override
+    public void transform(ITestAnnotation iTestAnnotation,
+                          Class aClass, Constructor constructor, Method method) {
+        IRetryAnalyzer retry = iTestAnnotation.getRetryAnalyzer();
+
+        if (retry == null) {
+            iTestAnnotation.setRetryAnalyzer(Retry.class);
+        }
+    }
+}
