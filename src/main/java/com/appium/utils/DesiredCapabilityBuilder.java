3075df4b779841d17339cb561691caac4be467ef
Shridhar Kalagi <shridharkalagi@gmail.com>
2017-10-26 18:24:34 +0530

    Fix for reading the user specified CAPS file

--- src/main/java/com/appium/utils/DesiredCapabilityBuilder.java
+++ src/main/java/com/appium/utils/DesiredCapabilityBuilder.java
@@ -42,7 +42,7 @@ public class DesiredCapabilityBuilder {
         return desiredCapabilitiesThreadLocal.get();
     }
 
-    public DesiredCapabilities buildDesiredCapability(String platform,
+    public void buildDesiredCapability(String platform,
                                                       String jsonPath) throws Exception {
         final boolean[] flag = {false};
         System.out.println("DeviceMappy-----" + DeviceAllocationManager
@@ -129,7 +129,6 @@ public class DesiredCapabilityBuilder {
         desiredCapabilities.setCapability(MobileCapabilityType.UDID,
                 DeviceManager.getDeviceUDID());
         desiredCapabilitiesThreadLocal.set(desiredCapabilities);
-        return desiredCapabilities;
     }
 
     public void appPackage(DesiredCapabilities desiredCapabilities) {
