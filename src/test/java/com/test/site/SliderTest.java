76c144b9b907b42c4d2c77e787ee30c127f1e6ff
Srinivasan Sekar <srinivasan.sekar1990@gmail.com>
2018-07-14 01:06:06 +0530

    Fix #357

--- src/test/java/com/test/site/SliderTest.java
+++ src/test/java/com/test/site/SliderTest.java
@@ -28,4 +28,34 @@ public class SliderTest extends  UserBaseTest {
         new ScreenShotManager().captureScreenShot("SliderScreen");
         swipe.perform();
     }
+
+    @Test
+    public void slider1Test() throws IOException, InterruptedException {
+        login("login").click();
+        waitForElement("slider1").click();
+        MobileElement slider = (MobileElement) waitForElement("slider");
+        Dimension size = slider.getSize();
+        TouchAction swipe = new TouchAction(driver).press(ElementOption.element(slider,
+                0, size.height / 2))
+                .waitAction(WaitOptions.waitOptions(Duration.ofSeconds(3)))
+                .moveTo(ElementOption.element(slider, size.width / 2,
+                        size.height / 2)).release();
+        new ScreenShotManager().captureScreenShot("SliderScreen");
+        swipe.perform();
+    }
+
+    @Test
+    public void slider2Test() throws IOException, InterruptedException {
+        login("login").click();
+        waitForElement("slider").click();
+        MobileElement slider = (MobileElement) waitForElement("slider");
+        Dimension size = slider.getSize();
+        TouchAction swipe = new TouchAction(driver).press(ElementOption.element(slider,
+                0, size.height / 2))
+                .waitAction(WaitOptions.waitOptions(Duration.ofSeconds(3)))
+                .moveTo(ElementOption.element(slider, size.width / 2,
+                        size.height / 2)).release();
+        new ScreenShotManager().captureScreenShot("SliderScreen");
+        swipe.perform();
+    }
 }
