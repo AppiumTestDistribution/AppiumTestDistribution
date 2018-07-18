6413188c2cbc4b007f5db727d5b0045dcb61f0a1
Shridhar Kalagi <shridharkalagi@gmail.com>
2018-07-18 12:33:21 +0530

    Fix #355 and #352 (#361)
    * Fix #355 and #352
    
    * Throw the exception instead of sysout

--- src/main/java/com/appium/utils/ScreenShotManager.java
+++ src/main/java/com/appium/utils/ScreenShotManager.java
@@ -5,19 +5,21 @@ import com.appium.entities.MobilePlatform;
 import com.appium.filelocations.FileLocations;
 import com.appium.manager.AppiumDeviceManager;
 import com.appium.manager.AppiumDriverManager;
+import com.epam.reportportal.service.ReportPortal;
 import org.apache.commons.io.FileUtils;
 import org.im4java.core.IM4JavaException;
 import org.openqa.selenium.OutputType;
 import org.testng.ITestResult;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.time.format.FormatStyle;
-import java.time.temporal.ChronoUnit;
+import java.util.Date;
 import java.util.Locale;
 
 /**
@@ -122,7 +124,7 @@ public class ScreenShotManager {
                 "screenshot/" + platform + "/" + udid
                         + "/" + className + "/"
                         + methodName + "/"
-                        + screenShotNameWithTimeStamp  + "-"
+                        + screenShotNameWithTimeStamp + "-"
                         + screenShotName + "_failed" + ".jpeg");
         setCapturedScreen(
                 "screenshot/" + platform + "/" + udid
@@ -138,7 +140,7 @@ public class ScreenShotManager {
                 + screenShotName + "_results.jpeg");
         setFramedFailedScreen(
                 "screenshot/" + platform + "/" + udid
-                        + "/" + className  + "/" + methodName + "/"
+                        + "/" + className + "/" + methodName + "/"
                         + screenShotNameWithTimeStamp + "-"
                         + screenShotName + "_failed.jpeg");
 
@@ -150,8 +152,14 @@ public class ScreenShotManager {
                 FileUtils.copyFile(scrFile, new File(System.getProperty("user.dir")
                         + FileLocations.OUTPUT_DIRECTORY + getFailedScreen().trim()));
             } else {
-                FileUtils.copyFile(scrFile, new File(System.getProperty("user.dir")
-                        + FileLocations.OUTPUT_DIRECTORY + getCapturedScreen().trim()));
+                String capturedScreenshotPath = System.getProperty("user.dir")
+                        + FileLocations.OUTPUT_DIRECTORY + getCapturedScreen().trim();
+                FileUtils.copyFile(scrFile, new File(capturedScreenshotPath));
+                if (new File(capturedScreenshotPath).exists()) {
+                    ReportPortal.emitLog(screenShotName, "Trace", new Date(), new File(capturedScreenshotPath));
+                } else {
+                    throw new FileNotFoundException();
+                }
             }
 
             File[] files1 = framePath.listFiles();
