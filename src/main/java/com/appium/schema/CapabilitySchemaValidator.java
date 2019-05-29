97f11b580c24e51f0f100e320a6ddfa32397e862
Sai <saikrishna321@yahoo.com>
2019-05-29 16:29:03 +0100

    backward isCloud

--- src/main/java/com/appium/schema/CapabilitySchemaValidator.java
+++ src/main/java/com/appium/schema/CapabilitySchemaValidator.java
@@ -74,8 +74,12 @@ public class CapabilitySchemaValidator {
                 JSONArray hostMachines = capabilityManager.getHostMachineObject();
                 for (Object hostMachine : hostMachines) {
                     JSONObject hostMachineJson = ((JSONObject) hostMachine);
+                    boolean isCloud = hostMachineJson.has("isCloud");
+                    if (isCloud) {
+                        isCloud = hostMachineJson.getBoolean("isCloud");
+                    }
                     String machineIP = (String) hostMachineJson.get("machineIP");
-                    if (hostMachineJson.getBoolean("isCloud")
+                    if (isCloud
                         || InetAddress.getByName(machineIP).isReachable(5000)) {
                         System.out.println("ATD is Running on " + machineIP);
                     } else {
