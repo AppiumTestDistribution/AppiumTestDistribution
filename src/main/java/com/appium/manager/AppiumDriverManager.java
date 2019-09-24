0d5a34589a1a038975d85e42dde41f75ea8ce5d2
Anand <abagmar@gmail.com>
2019-09-24 17:48:49 +0530

    Make initialiseDriver a synchronised call

--- src/main/java/com/appium/manager/AppiumDriverManager.java
+++ src/main/java/com/appium/manager/AppiumDriverManager.java
@@ -10,6 +10,7 @@ import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.ios.IOSDriver;
 import io.appium.java_client.remote.AndroidMobileCapabilityType;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import lombok.Synchronized;
 import org.apache.commons.lang3.ArrayUtils;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
@@ -42,6 +43,7 @@ public class AppiumDriverManager {
         appiumDriver.set(driver);
     }
 
+    @Synchronized
     private AppiumDriver<MobileElement> initialiseDriver(
         Optional<DesiredCapabilities> capabilities)
         throws Exception {
@@ -121,6 +123,7 @@ public class AppiumDriverManager {
         return appiumManager.getRemoteWDHubIP(hostName);
     }
 
+    @Synchronized
     private void startAppiumDriverInstance(Optional<DesiredCapabilities> desiredCapabilities)
         throws Exception {
         AppiumDriver<MobileElement> currentDriverSession;
