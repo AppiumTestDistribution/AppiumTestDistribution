2729238ea21ad76ff9df6ccb54541e66be1adf6c
WasiqB <wasbhamla2005@gmail.com>
2019-12-14 13:01:40 +0530

    Updated outdated dependency and fixed build warning.

--- src/test/java/com/test/site/Runner.java
+++ src/test/java/com/test/site/Runner.java
@@ -10,16 +10,9 @@ import java.util.List;
 public class Runner {
     @Test
     public static void testApp() throws Exception {
-        //udid="32085b6d8fdb71ff,FA66VBN01980" mvn clean -Dtest=Runner test
-        /*String udidParam = System.getenv("udid");
-        String[] parts = udidParam.split(",");
-        List<String> deviceList = Arrays.asList(parts);
-        System.out.println(deviceList);*/
         List<String> tests = new ArrayList<>();
-        //tests.add("HomePageTest2");
         tests.add("SliderTest");
         tests.add("DragnDropTest");
-        //tests.add("SliderTest");
         ATDRunner atdRunner = new ATDRunner();
         boolean hasFailures = atdRunner.runner("com.test.site", tests);
         Assert.assertFalse(hasFailures, "Testcases have failed in parallel execution");
