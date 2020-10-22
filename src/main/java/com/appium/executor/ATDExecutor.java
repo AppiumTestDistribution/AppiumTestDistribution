bba3443ab8acdbd8c1d48697bfc157955888d83b
Srinivasan Sekar <srinivasan.sekar1990@gmail.com>
2020-10-22 11:18:53 +0530

    fix checkstyle errors

--- src/main/java/com/appium/executor/ATDExecutor.java
+++ src/main/java/com/appium/executor/ATDExecutor.java
@@ -23,7 +23,14 @@ import java.io.IOException;
 import java.lang.reflect.Method;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.*;
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 import static com.appium.filelocations.FileLocations.PARALLEL_XML_LOCATION;
 import static com.appium.utils.ConfigFileManager.*;
