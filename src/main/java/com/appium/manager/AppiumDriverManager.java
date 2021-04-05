66a585a79176a46a445caa1d16ddf45dc11545ef
rakurs987@gmail.com <rakurs987@gmail.com>
2021-04-05 17:58:38 +0530

    Upgraded chromeDriver version and add chromeDriverPort for android parallel

--- src/main/java/com/appium/manager/AppiumDriverManager.java
+++ src/main/java/com/appium/manager/AppiumDriverManager.java
@@ -132,8 +132,8 @@ public class AppiumDriverManager {
                 + " on device: " + id;
             LOGGER.info(message);
             WebDriverManager.chromedriver()
-                .version(String.valueOf(highestChromeVersion)).setup();
-            return WebDriverManager.chromedriver().getBinaryPath();
+                .browserVersion(String.valueOf(highestChromeVersion)).setup();
+            return WebDriverManager.chromedriver().getDownloadedDriverPath();
         } else {
             return null;
         }
