d4a67609a8017636fad93eeab953e0e050e33847
Anand Bagmar <abagmar@gmail.com>
2021-05-09 11:54:21 +0530

    set Platform, config file and capabilities file for Windows test

--- src/test/java/com/test/site/WindowsRunner.java
+++ src/test/java/com/test/site/WindowsRunner.java
@@ -12,6 +12,9 @@ public class WindowsRunner {
     public static void testApp() throws Exception {
         List<String> tests = new ArrayList<>();
         tests.add("WindowsTest");
+        System.setProperty("Platform", "windows");
+        System.setProperty("CONFIG_FILE", "./configs/windowsconfig.properties");
+        System.setProperty("CAPS", "./caps/windows.json");
         ATDRunner atdRunner = new ATDRunner();
         boolean hasFailures = atdRunner.runner("com.test.site", tests);
         Assert.assertFalse(hasFailures, "Testcases have failed in parallel execution");
