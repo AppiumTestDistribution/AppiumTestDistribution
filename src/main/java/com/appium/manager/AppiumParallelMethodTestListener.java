7616e8bf586e61b743772e29652dbd0404501388
Anil <anilpatidar36@gmail.com>
2022-11-25 18:33:47 +0530

    Update AppiumParallelMethodTestListener.java

--- src/main/java/com/appium/manager/AppiumParallelMethodTestListener.java
+++ src/main/java/com/appium/manager/AppiumParallelMethodTestListener.java
@@ -93,7 +93,6 @@ public final class AppiumParallelMethodTestListener extends Helpers
      */
     @Override
     public void beforeInvocation(IInvokedMethod iInvokedMethod, ITestResult iTestResult) {
-        System.out.println("inside ATD:beforeInvocation");
         String testMethodName = iInvokedMethod.getTestMethod().getMethodName();
         allocateDeviceAndStartDriver(testMethodName, iTestResult);
         if (!isCloudExecution()) {
@@ -122,7 +121,6 @@ public final class AppiumParallelMethodTestListener extends Helpers
 
     private void allocateDeviceAndStartDriver(String testMethodName, ITestResult iTestResult) {
         try {
-                    System.out.println("inside start of ATD:allocateDeviceAndStartDriver");
             AppiumDriver driver = AppiumDriverManager.getDriver();
             if (driver == null || driver.getSessionId() == null) {
                 deviceAllocationManager.allocateDevice(deviceAllocationManager
@@ -135,8 +133,6 @@ public final class AppiumParallelMethodTestListener extends Helpers
         } catch (Exception e) {
             e.printStackTrace();
         }
-                System.out.println("inside end of ATD:allocateDeviceAndStartDriver");
-
     }
 
     /*
